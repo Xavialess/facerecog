53d11175a0deeeed0677d36c56302062818e15c3
Xavier Quintanilla <xquintan@ualberta.ca>
2023-07-24 01:29:05 -0500

    fix /image route

--- src/App.js
+++ src/App.js
@@ -132,14 +132,14 @@ class App extends Component {
 
   onButtonSubmit = () => {
     this.setState({ imageURL: this.state.input });
-    fetch("https://api.clarifai.com/v2/models/" + "face-detection" + "/outputs", returnClarifyRequestOptions(this.state.input))
+    fetch("https://api.clarifai.com/v2/models/face-detection/outputs", returnClarifyRequestOptions(this.state.input))
       .then(response => response.json())
       .then(response => {
         console.log('hi', response);
         if (response) {
           fetch('http://localhost:3000/image', {
             method: 'put',
-            headers: { 'Content-Type': 'application.json' },
+            headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({
               id: this.state.user.id
             })
