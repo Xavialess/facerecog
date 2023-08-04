6c1068a3b5b07affb5dc97c3b4016d5a37b38def
Xavier Quintanilla <xquintan@ualberta.ca>
2023-08-04 15:57:54 -0500

    Commits before deployment

--- src/App.js
+++ src/App.js
@@ -145,7 +145,7 @@ class App extends Component {
       .then(response => {
         console.log('hi', response);
         if (response) {
-          fetch('http://localhost:3000/image', {
+          fetch('https://facerecognition-backend-mz50.onrender.com/image', {
             method: 'put',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({
--- src/Components/Signin/Signin.js
+++ src/Components/Signin/Signin.js
@@ -18,7 +18,7 @@ class Signin extends React.Component {
     }
 
     onSubmitSignIn = () => {
-        fetch('http://localhost:3000/signin', {
+        fetch('https://facerecognition-backend-mz50.onrender.com/signin', {
             method: 'post',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
